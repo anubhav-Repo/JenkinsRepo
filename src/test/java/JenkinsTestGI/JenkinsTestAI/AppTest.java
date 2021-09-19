d67e4992cabe78a76891c8dfa97185f9c857057f
Anubhav <ankit.k@qapitol.com>
2021-09-19 22:05:03 +0530

    First Commit of Test case

---
+++ src/test/java/JenkinsTestGI/JenkinsTestAI/AppTest.java
@@ -0,0 +1,16 @@
+package JenkinsTestGI.JenkinsTestAI;
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+	
+	@Test
+	public void firstTestCase(){
+		System.out.println("First Test case");
+	}
+	
+	
+}
